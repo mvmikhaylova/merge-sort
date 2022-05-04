6902be095f3070997b585d1f531784cb669ecd0d
mvmikhaylova <91223620+mvmikhaylova@users.noreply.github.com>
2022-05-04 21:47:01 +0300

    Add files via upload

---
+++ cmake-build-debug/merge-sort/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/mihai/CLionProjects/merge-sort-mvmikhaylova/merge-sort
+# Build directory: C:/Users/mihai/CLionProjects/merge-sort-mvmikhaylova/cmake-build-debug/merge-sort
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(merge-sort-test "C:/Users/mihai/CLionProjects/merge-sort-mvmikhaylova/cmake-build-debug/merge-sort/merge-sort-test.exe" "--force-colors" "-d")
+set_tests_properties(merge-sort-test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/mihai/CLionProjects/merge-sort-mvmikhaylova/merge-sort/CMakeLists.txt;4;add_test;C:/Users/mihai/CLionProjects/merge-sort-mvmikhaylova/merge-sort/CMakeLists.txt;0;")
