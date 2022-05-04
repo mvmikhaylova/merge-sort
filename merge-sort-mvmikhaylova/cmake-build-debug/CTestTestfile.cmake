bc6a200465d6a64832bf01d82355e57416106048
mvmikhaylova <91223620+mvmikhaylova@users.noreply.github.com>
2022-05-04 21:46:01 +0300

    Delete merge-sort-mvmikhaylova directory

--- merge-sort-mvmikhaylova/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mihai/CLionProjects/merge-sort-mvmikhaylova
-# Build directory: C:/Users/mihai/CLionProjects/merge-sort-mvmikhaylova/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/doctest")
-subdirs("merge-sort")
--- merge-sort-mvmikhaylova/merge-sort/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "merge-sort.hpp"
-#include <iostream>
-#include <string>
-
-int main() {
-    return 0;
-}
